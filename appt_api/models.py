ad05fb377eabae04c8e013623ea1b453ea43d0bc
culestfrosh <culestfrosh@gmail.com>
2022-11-02 20:23:08 +0100

    update func

--- appt_api/models.py
+++ appt_api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class User:
-  
-class Doctor:
-class Patient:
-class Appointment:
+class User(models.Model):
+  name = models.CharField(max_length=30)
+  def __str__(self):
+    return self.name
+#class Doctor:
+#class Patient:
+#class Appointment:
